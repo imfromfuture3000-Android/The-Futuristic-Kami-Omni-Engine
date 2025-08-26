4c8cf9028450b82028645722914e9138bc94ec9a
cercenialoyd-dotcom <cercenialoyd@gmail.com>
2025-08-26 22:01:14 +0800

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const { PRIVATE_KEY, SCALE_RPC_URL, CHAIN_ID } = process.env;
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
     skale: {
-      url: process.env.RPC_URL,       // SKALE RPC endpoint
-      accounts: [process.env.PRIVATE_KEY] // Wallet private key
-    }
-  }
+      url: SCALE_RPC_URL || "",
+      chainId: Number(CHAIN_ID) || 2046399126,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      gas: "auto",
+      gasPrice: 0, // SKALE uses SFUEL (free gas)
+    },
+  },
+  etherscan: {
+    apiKey: {
+      skale: "not-needed", // SKALE explorer doesn't require API key
+    },
+    customChains: [
+      {
+        network: "skale",
+        chainId: 2046399126,
+        urls: {
+          apiURL: "https://elated-tan-skale.explorer.mainnet.skalenodes.com/api",
+          browserURL: "https://elated-tan-skale.explorer.mainnet.skalenodes.com",
+        },
+      },
+    ],
+  },
 };
